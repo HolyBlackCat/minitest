49fd5c109df2673cc84a0e21fc8e51a70c3f0626
HolyBlackCat <holyblckcat@gmail.com>
2025-10-03 20:34:13 +0500

    Add an optional source file that defines the main function.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// You can optionally use this file to provide the `main()` function for tests,
+//   if you don't want to define it in your own code.
+
+#define EM_ENABLE_TESTS 1 // Using `1` to not conflict with the `-DENABLE_TESTS`.
+#define EM_MINITEST_IMPLEMENTATION
+#include <em/minitest.hpp>
+
+EM_MINITEST_MAIN
